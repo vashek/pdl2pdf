e065ce25e60408f0620eba23a43828000f24e3ca
Dvořák Václav <Vaclav.Dvorak@ysoft.com>
2020-03-23 16:19:21 +0100

    workaround for https://github.com/anthony-tuininga/cx_Freeze/issues/591

--- setup_cx.py
+++ setup_cx.py
@@ -1,7 +1,22 @@
+import glob
+import os.path
+import sys
+
 from setuptools.config import read_configuration
 
 
 if __name__ == "__main__":
+    data_files = []
+    for dll_mask in ["libcrypto-*.dll", "libssl-*.dll", "libffi-7.dll"]:
+        for dir_name in sys.path:
+            found_dlls = glob.glob(os.path.join(dir_name, dll_mask))
+            if found_dlls:
+                data_files.append(sorted(found_dlls)[-1])
+                print(f"... {data_files[-1]} found ...")
+                break
+        else:
+            print(f"!!! {dll_mask} not found !!!")
+
     cfg = read_configuration("setup.cfg")
     # perhaps use cfg["options"]["packages"]?
     cfg = cfg["metadata"]
@@ -11,10 +26,13 @@ if __name__ == "__main__":
                 "test",
                 "tkinter",
             ],
+            "include_files": data_files,
         },
     }
 
+    import cx_Freeze.hooks
     from cx_Freeze import setup, Executable
+    del cx_Freeze.hooks.load__ctypes
     setup(
         executables=[
             Executable("pdl2pdf.py"),
