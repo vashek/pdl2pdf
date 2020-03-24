3e876d4a24310d7f1da4fc2b5a8c6e73d381bf7e
Dvořák Václav <Vaclav.Dvorak@ysoft.com>
2020-03-24 01:08:46 +0100

    implement the conversion

--- pdl2pdf.py
+++ pdl2pdf.py
@@ -1,13 +1,29 @@
 """A command-line tool to convert a print job to PDF using Ghostscript/GhostPCL"""
 __version__ = "0.1.0"
 
+import datetime
+import os.path
 import subprocess
 import sys
 from enum import Enum, auto, unique
 from pathlib import Path
-from typing import Type, BinaryIO
+from typing import Optional, Type
 
 import click
+
+# noinspection SpellCheckingInspection
+SAFE_CHARS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_,.-=+!@$()"
+
+
+def make_safe_file_name_part(user_input: str, replacement: str = "_") -> str:
+    """Replace any characters unsafe for a file name"""
+    result = ""
+    for char in user_input:
+        if char in SAFE_CHARS:
+            result += char
+        else:
+            result += replacement
+    return result
 
 
 @unique
@@ -41,16 +57,23 @@ def dir_with_this_exe() -> Path:
     return this_exe().parent.resolve()
 
 
+def get_gpcl_exe() -> str:
+    """Return full path to GhostPCL exe file"""
+    return str(dir_with_this_exe() / "gpcl" / "gpcl6win64.exe")
+
+
+def get_gs_exe() -> str:
+    """Return full path to Ghostscript exe file"""
+    return str(dir_with_this_exe() / "gs" / "bin" / "gswin64c.exe")
+
+
 # noinspection PyUnusedLocal
 def do_self_test(ctx, param, value):  # pylint: disable=unused-argument
     """Verifies that the program is able to run."""
     if value is True:
         click.echo("self-test started")
-        gpcl_exe = dir_with_this_exe() / "gpcl" / "gpcl6win64.exe"
-        # noinspection PyTypeChecker
-        subprocess.check_call([gpcl_exe], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-        gs_exe = dir_with_this_exe() / "gs" / "bin" / "gswin64c.exe"
-        subprocess.check_call([gs_exe, "-h"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        subprocess.check_call([get_gpcl_exe()], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        subprocess.check_call([get_gs_exe(), "-h"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         click.echo("self-test OK")
         sys.exit(0)
 
@@ -59,12 +82,35 @@ def do_self_test(ctx, param, value):  # pylint: disable=unused-argument
 @click.version_option(version=__version__)
 @click.option("--self-test", is_flag=True, is_eager=True, callback=do_self_test, help="Do nothing and exit with exit code 0.")
 @click.option("--job-language", type=ClickChoiceEnum(JobLanguage, case_sensitive=False), required=True, help="Print job language.")
+@click.option("--title", help="Optional text to append to the output file name.")
+@click.option("--timeout", metavar="S", type=click.IntRange(min=1), help="Optional time limit (in seconds) for the conversion to run.")
 @click.argument("input_fn", metavar="INPUT", type=click.Path(exists=True, dir_okay=False, resolve_path=True, allow_dash=True))
-@click.argument("output_f", metavar="OUTPUT", type=click.File("wb"))
-def pdl2pdf(self_test: bool, job_language: JobLanguage, input_fn: str, output_f: BinaryIO):
-    """Make a PDF file called <OUTPUT> out of print job file <INPUT>."""
+@click.argument("output_dir", metavar="OUTPUT_DIR", type=click.Path(exists=True, file_okay=False, dir_okay=True, writable=True))
+def pdl2pdf(
+    self_test: bool, job_language: JobLanguage, title: Optional[str], timeout: Optional[int], input_fn: str, output_dir: str
+):  # pylint: disable=too-many-arguments
+    """Make a PDF file in <OUTPUT_DIR> out of print job file <INPUT>."""
     assert not self_test
-    click.echo(f"Using job language {job_language}, convert from {input_fn} to {output_f.name}")
+    # this gives YYYYmmdd_HHMMSS_xxx where xxx means milliseconds - reasonably sure to be unique but still readable
+    timestamp = datetime.datetime.now().isoformat("_", timespec="milliseconds").replace(":", "").replace("-", "").replace(".", "_")
+    if title:
+        output_fn = f"{timestamp}-{make_safe_file_name_part(title)}.pdf"
+    else:
+        output_fn = f"{timestamp}.pdf"
+    full_output_fn = os.path.join(output_dir, output_fn)
+    click.echo(f"Converting from {job_language} file {input_fn} to {full_output_fn}")
+    if job_language == JobLanguage.PCL:
+        program = get_gpcl_exe()
+    elif job_language == JobLanguage.PS:
+        program = get_gs_exe()
+    else:
+        raise RuntimeError("unknown language")
+    params = [program, "-sPAPERSIZE=a4", "-sDEVICE=pdfwrite", "-o", full_output_fn, input_fn]
+    subprocess.check_call(params, timeout=timeout)
+    if os.path.isfile(full_output_fn):
+        click.echo(f"Successfully produced {full_output_fn}")
+    else:
+        click.echo(f"Something went wrong; after running the converter, there is no {full_output_fn}")
 
 
 if __name__ == "__main__":
